f21016827cef878440fe9b0773bf922a59e4ce48
Hardik Gupta <94279016+hardikgupta2003@users.noreply.github.com>
2023-07-03 09:52:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ function handleSlider(){
 function setIndicator(color){
     indicator.style.backgroundColor=color;
     //shadow
+    indicator.style.boxShadow = `0px 0px 12px 1px ${color}` ;
 }
 
 function getRndInteger(min,max){
