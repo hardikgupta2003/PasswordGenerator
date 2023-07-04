3ee0d0b3ddd9e6e5984e766efa945f4449054924
Hardik Gupta <94279016+hardikgupta2003@users.noreply.github.com>
2023-07-05 00:16:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,6 +26,10 @@ setIndicator('#ccc')
 function handleSlider() {
     inputSlider.value = passwordLength;
     lengthDisplay.innerText = passwordLength;
+
+    const min = inputSlider.min;
+    const max = inputSlider.max;
+    inputSlider.style.backgroundSize = ( (passwordLength - min)*100/(max - min)) + "% 100%"
 }
 
 function setIndicator(color) {
